7e67f8dc1615773dee6430d98161ee4741d89ad9
Elcelcior <tototobey@gmail.com>
2024-10-02 18:54:45 -0400

    Added Transparency Slider, and failed Rotation

--- lib/main.dart
+++ lib/main.dart
@@ -19,13 +19,11 @@ class FadingTextAnimation extends StatefulWidget {
 }
 
 class _FadingTextAnimationState extends State<FadingTextAnimation> {
+  double _currentSliderValue = 1;
   bool _isVisible = true;
-
-  void toggleVisibility() {
-    setState(() {
-      _isVisible = !_isVisible;
-    });
-  }
+  Image myImage = Image.network(
+    'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/d3789c8c-0874-407c-a457-03b147f59b18/deq4bhq-e1c4ce2a-d32b-42e7-9cbe-d764c2201538.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2QzNzg5YzhjLTA4NzQtNDA3Yy1hNDU3LTAzYjE0N2Y1OWIxOFwvZGVxNGJocS1lMWM0Y2UyYS1kMzJiLTQyZTctOWNiZS1kNzY0YzIyMDE1MzgucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.XOoSJDCA9vikjYTpSuQLA4bCQ6467YrbPSVd7t28xQs',
+  );
 
   @override
   Widget build(BuildContext context) {
@@ -33,19 +31,36 @@ class _FadingTextAnimationState extends State<FadingTextAnimation> {
       appBar: AppBar(
         title: Text('Fading Text Animation'),
       ),
-      body: Center(
-        child: AnimatedOpacity(
-          opacity: _isVisible ? 1.0 : 0.0,
-          duration: Duration(seconds: 1),
-          child: Text(
-            'Hello, Flutter!',
-            style: TextStyle(fontSize: 24),
+      body: Column(
+        children: [
+          Center(
+            child: AnimatedOpacity(
+              opacity: _currentSliderValue,
+              duration: Duration(seconds: 0),
+              child: myImage,
+            ),
           ),
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: toggleVisibility,
-        child: Icon(Icons.play_arrow),
+          Slider(
+            value: _currentSliderValue,
+            max: 1,
+            label: _currentSliderValue.round().toString(),
+            onChanged: (double value) {
+              setState(() {
+                _currentSliderValue = value;
+              });
+            },
+          ),
+          AnimatedContainer(
+            height: 200,
+            curve: Curves.bounceIn,
+            duration: const Duration(milliseconds: 200),
+            child: AnimatedOpacity(
+              opacity: _currentSliderValue,
+              duration: Duration(seconds: 0),
+              child: myImage,
+            ),
+          )
+        ],
       ),
     );
   }
